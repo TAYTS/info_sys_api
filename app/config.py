71d2b9c0f62b577d2d4611132ba9ce276e620cbb
TAY TS <taytzushieh@gmail.com>
2018-10-31 17:00:06 +0800

    feature: Add the app session setting to the config

--- app/config.py
+++ app/config.py
@@ -1,8 +1,9 @@
 """ Development Configurations """
+from datetime import timedelta
 
 # SQL
-SECRET_KEY = ''
-SQLALCHEMY_DATABASE_URI = ''
+SECRET_KEY = ""
+SQLALCHEMY_DATABASE_URI = ""
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 PREFERRED_URL_SCHEME = "https"
 
@@ -11,3 +12,7 @@ SESSION_TYPE = 'filesystem'
 # Log
 APP_LOG_FILE = 'log/app.log'
 APP_LOG_LEVEL = 'DEBUG'
+
+# App Session Setting
+SESSION_COOKIE_SECURE = True
+PERMANENT_SESSION_LIFETIME = timedelta(days=30)
