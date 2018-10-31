170e7d7f2bed22d293a7b92e1a8afa77c1001adf
TAY TS <taytzushieh@gmail.com>
2018-10-31 16:57:12 +0800

    feature: Add new endpoint for login and logout and update the register endpoint

--- app/modules/admin/__init__.py
+++ app/modules/admin/__init__.py
@@ -1,11 +1,15 @@
 from flask import Blueprint
 
 # Import all the view function
-from app.modules.admin.register import register_vendor, register_user
+from app.modules.admin.register import register, login, logout
 
 # Define the blueprint name
 module = Blueprint('admin', __name__)
 
 
-module.add_url_rule('/admin/register_vendor', view_func=register_vendor, methods=['POST'])
-module.add_url_rule('/admin/register_user', view_func=register_user, methods=['POST'])
+module.add_url_rule('/admin/register',
+                    view_func=register, methods=['POST'])
+module.add_url_rule('/admin/login',
+                    view_func=login, methods=['POST'])
+module.add_url_rule('/admin/logout',
+                    view_func=logout, methods=['GET'])
